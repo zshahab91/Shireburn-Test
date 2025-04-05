557911f81ee11365af751b8eb6b91e43c7782252
zahra shahab <zshahab91@gmail.com>
2025-04-05 19:49:27 +0200

    Add json server

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/employees">Employees</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/EmployeesView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="employees">
+    <h1>This is an employees page</h1>
+    <div v-for="employee in employees" :key="employee.id">
+      <h2>{{ employee.firstName }} {{ employee.lastName }}</h2>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const employees = ref([])
+
+onMounted(async () => {
+  try {
+    const response = await fetch('http://localhost:3000/employees')
+    employees.value = await response.json()
+  } catch (error) {
+    console.error('Error fetching employees:', error)
+  }
+})
+</script>
+<style scoped></style>
